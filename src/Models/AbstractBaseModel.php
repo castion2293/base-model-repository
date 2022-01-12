e8300471475c82c0e1c311f109180b1ad70ea2f2
Nick Zhang <castion2293@yahoo.com.tw>
2022-01-12 09:40:28 +0800

    fix: 增加AbstractBaseModel(@nick)

---
+++ src/Models/AbstractBaseModel.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AbstractBaseModel extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = ['id'];
+
+    /**
+     * 覆寫序列化方法 toArray()時使用
+     *
+     * @param DateTimeInterface $date
+     * @return string
+     */
+    protected function serializeDate(DateTimeInterface $date): string
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
+}
